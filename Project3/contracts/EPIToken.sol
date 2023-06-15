b837d377c83c4ccdd9426e56fd2edf37423637bf
Michael <gadreelazazel@gmail.com>
2023-06-15 14:17:26 +0100

    Update and Create

--- Project3/contracts/EPIToken.sol
+++ Project3/contracts/EPIToken.sol
@@ -4,37 +4,44 @@ pragma solidity ^0.8.9;
 
 contract EPITokenMint {
 
+    // Declare public variables
     string public tokenName;
     string public tokenSymbol;
-    uint public totalSupply;
+    uint256 public totalSupply;
     address public owner;
 
-    mapping(address => uint) public balance;
+    // Create a mapping of addresses to balances
+    mapping(address => uint256) public balance;
 
-    constructor(string memory _tokenName, string memory symbol) {
-        tokenName = _tokenName;
-        tokenSymbol = symbol;
+    // Constructor function that sets the token name, symbol, and owner
+    constructor() {
+        tokenName = "Eth Proof Intermediate Module 3 Token";
+        tokenSymbol = "EIM";
         owner = msg.sender;
     }
 
+    // Modifier that only allows the owner to execute a function
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can perform this action!");
         _;
     }
 
-    function mint(uint _amount) public onlyOwner {
+    // Function that allows the owner to mint new tokens
+    function mint(uint256 _amount) public onlyOwner {
         totalSupply += _amount;
         balance[msg.sender] += _amount;
     }
 
+    // Function that allows anyone to burn their own tokens
     function burn(uint256 _amount) public {
         totalSupply -= _amount;
         balance[msg.sender] -= _amount;
     }
 
-    function transfer(address _recipient, uint _amount) public {
+    // Function that allows anyone to transfer tokens to another address
+    function transfer(address _recipient, uint256 _amount) public {
         require(msg.sender != _recipient,"You can not transfer token(s) to yourself!");
-        assert(balance[msg.sender] >= _amount);
+        require(balance[msg.sender] >= _amount, "Transfer amount exceeds balance");
         balance[msg.sender] -= _amount;
         balance[_recipient] += _amount;
     }
